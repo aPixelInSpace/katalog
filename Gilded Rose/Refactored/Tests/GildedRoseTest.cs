cd26d0c07a15db3a3beb1fb51929b1f81876a726
Patrizio Amella <62834023+aPixelInSpace@users.noreply.github.com>
2022-07-06 23:31:52 +0200

    test(Gilded Rose): added

--- Gilded Rose/Refactored/Tests/GildedRoseTest.cs
+++ Gilded Rose/Refactored/Tests/GildedRoseTest.cs
@@ -1,15 +1,125 @@
 using App;
+using FluentAssertions;
 
 namespace Tests;
 
 public class GildedRoseTest
 {
-    [Fact]
-    public void foo()
+    [Theory]
+    [InlineData("foo", 2, 2, 1, 1)]
+    [InlineData("foo", 1, 1, 0, 0)]
+    public void NormalItem_Degrades(string name, int sellIn, int quality, int expectedSellIn, int expectedQuality)
     {
-        IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
-        GildedRose app = new GildedRose(Items);
+        var items = new List<Item> { new() { Name = name, SellIn = sellIn, Quality = quality } };
+        var app = new GildedRose(items);
         app.UpdateQuality();
-        Assert.Equal("fixme", Items[0].Name);
+        
+        items[0].Name.Should().Be(name);
+        items[0].SellIn.Should().Be(expectedSellIn);
+        items[0].Quality.Should().Be(expectedQuality);
+    }
+    
+    [Theory]
+    [InlineData("foo", 0, 0, -1, 0)]
+    [InlineData("foo", -1, 0, -2, 0)]
+    // The Quality of an item is never negative
+    public void NormalItem_StopsDegrading(string name, int sellIn, int quality, int expectedSellIn, int expectedQuality)
+    {
+        var items = new List<Item> { new() { Name = name, SellIn = sellIn, Quality = quality } };
+        var app = new GildedRose(items);
+        app.UpdateQuality();
+        
+        items[0].Name.Should().Be(name);
+        items[0].SellIn.Should().Be(expectedSellIn);
+        items[0].Quality.Should().Be(expectedQuality);
+    }
+    
+    [Theory]
+    [InlineData("foo", 0, 4, -1, 2)]
+    [InlineData("foo", 0, 1, -1, 0)]
+    [InlineData("foo", 0, 0, -1, 0)]
+    // Once the sell by date has passed, Quality degrades twice as fast
+    // The Quality of an item is never negative
+    public void NormalItem_DegradesTwiceAsFast(string name, int sellIn, int quality, int expectedSellIn, int expectedQuality)
+    {
+        var items = new List<Item> { new() { Name = name, SellIn = sellIn, Quality = quality } };
+        var app = new GildedRose(items);
+        app.UpdateQuality();
+
+        items[0].Name.Should().Be(name);
+        items[0].SellIn.Should().Be(expectedSellIn);
+        items[0].Quality.Should().Be(expectedQuality);
+    }
+    
+    [Theory]
+    [InlineData("Aged Brie", 2, 0, 1, 1)]
+    [InlineData("Aged Brie", 0, 49, -1, 50)]
+    [InlineData("Aged Brie", 0, 50, -1, 50)]
+    [InlineData("Aged Brie", -1, 50, -2, 50)]
+    // "Aged Brie" actually increases in Quality the older it gets
+    // The Quality of an item is never more than 50
+    public void AgedBrie_IncreasesInQuality(string name, int sellIn, int quality, int expectedSellIn, int expectedQuality)
+    {
+        var items = new List<Item> { new() { Name = name, SellIn = sellIn, Quality = quality } };
+        var app = new GildedRose(items);
+        app.UpdateQuality();
+
+        items[0].Name.Should().Be(name);
+        items[0].SellIn.Should().Be(expectedSellIn);
+        items[0].Quality.Should().Be(expectedQuality);
+    }
+    
+    [Theory]
+    [InlineData("Sulfuras, Hand of Ragnaros", 2, 80, 2, 80)]
+    [InlineData("Sulfuras, Hand of Ragnaros", 0, 80, 0, 80)]
+    [InlineData("Sulfuras, Hand of Ragnaros", -1, 80, -1, 80)]
+    [InlineData("Sulfuras, Hand of Ragnaros", -2, 80, -2, 80)]
+    // "Sulfuras", being a legendary item, never has to be sold or decreases in Quality
+    // "Sulfuras" is a legendary item and as such its Quality is 80 and it never alters.
+    public void Sulfuras_NeverAlters(string name, int sellIn, int quality, int expectedSellIn, int expectedQuality)
+    {
+        var items = new List<Item> { new() { Name = name, SellIn = sellIn, Quality = quality } };
+        var app = new GildedRose(items);
+        app.UpdateQuality();
+
+        items[0].Name.Should().Be(name);
+        items[0].SellIn.Should().Be(expectedSellIn);
+        items[0].Quality.Should().Be(expectedQuality);
+    }
+    
+    [Theory]
+    [InlineData("Backstage passes to a TAFKAL80ETC concert", 11, 1, 10, 2)]
+    [InlineData("Backstage passes to a TAFKAL80ETC concert", 10, 1, 9, 3)]
+    [InlineData("Backstage passes to a TAFKAL80ETC concert", 9, 3, 8, 5)]
+    [InlineData("Backstage passes to a TAFKAL80ETC concert", 6, 3, 5, 5)]
+    [InlineData("Backstage passes to a TAFKAL80ETC concert", 5, 3, 4, 6)]
+    [InlineData("Backstage passes to a TAFKAL80ETC concert", 2, 8, 1, 11)]
+    [InlineData("Backstage passes to a TAFKAL80ETC concert", 1, 8, 0, 11)]
+    [InlineData("Backstage passes to a TAFKAL80ETC concert", 0, 8, -1, 0)]
+    // "Backstage passes", like aged brie, increases in Quality as its SellIn value approaches; Quality increases by 2
+    // when there are 10 days or less and by 3 when there are 5 days or less but Quality drops to 0 after the concert
+    public void BackstagePasses_IncreasesInQualityBeforeSellInThen0After(string name, int sellIn, int quality, int expectedSellIn, int expectedQuality)
+    {
+        var items = new List<Item> { new() { Name = name, SellIn = sellIn, Quality = quality } };
+        var app = new GildedRose(items);
+        app.UpdateQuality();
+
+        items[0].Name.Should().Be(name);
+        items[0].SellIn.Should().Be(expectedSellIn);
+        items[0].Quality.Should().Be(expectedQuality);
+    }
+    
+    [Theory]
+    [InlineData("Conjured Mana Cake", 4, 4, 3, 2)]
+    // "Conjured" items degrade in Quality twice as fast as normal items
+    public void Conjured_DecreasesInQualityTwiceAsFastAsNormalItem(string name, int sellIn, int quality, int expectedSellIn, int expectedQuality)
+    {
+        var items = new List<Item> { new() { Name = name, SellIn = sellIn, Quality = quality } };
+        var app = new GildedRose(items);
+        app.UpdateQuality();
+
+        items[0].Name.Should().Be(name);
+        items[0].SellIn.Should().Be(expectedSellIn);
+        items[0].Quality.Should().Be(expectedQuality);
     }
 }
