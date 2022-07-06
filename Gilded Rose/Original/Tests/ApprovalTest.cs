b88a685363990e79e1d415a76d7a224b611108a3
Patrizio Amella <62834023+aPixelInSpace@users.noreply.github.com>
2022-07-06 21:43:35 +0200

    init : copy and scaffolding of the Gilded Rose kata

---
+++ Gilded Rose/Original/Tests/ApprovalTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+
+namespace Tests;
+
+[UsesVerify]
+public class ApprovalTest
+{
+    [Fact]
+    public Task ThirtyDays()
+    {
+        var fakeoutput = new StringBuilder();
+        Console.SetOut(new StringWriter(fakeoutput));
+        Console.SetIn(new StringReader("a\n"));
+
+        Program.Main(new string[] { });
+        var output = fakeoutput.ToString();
+
+        return Verifier.Verify(output);
+    }
+}
